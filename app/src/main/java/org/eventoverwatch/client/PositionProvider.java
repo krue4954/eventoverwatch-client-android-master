240d3fdbff79b9a9dcadd223766a07a513bba345
Michael Krueger <mkrueger@Michaels-MacBook-Pro.local>
2016-07-09 16:10:36 -0400

    Initial commit

---
+++ app/src/main/java/org/eventoverwatch/client/PositionProvider.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2013 - 2015 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eventoverwatch.client;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public abstract class PositionProvider {
+
+    protected static final String TAG = PositionProvider.class.getSimpleName();
+
+    public interface PositionListener {
+        void onPositionUpdate(Position position);
+    }
+
+    private final PositionListener listener;
+
+    private final Context context;
+    private final SharedPreferences preferences;
+    protected final LocationManager locationManager;
+
+    private String deviceId;
+    protected String type;
+    protected final long period;
+
+    private long lastUpdateTime;
+
+    public PositionProvider(Context context, PositionListener listener) {
+        this.context = context;
+        this.listener = listener;
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        deviceId = preferences.getString(MainActivity.KEY_DEVICE, null);
+        period = Integer.parseInt(preferences.getString(MainActivity.KEY_INTERVAL, null)) * 1000;
+
+        type = preferences.getString(MainActivity.KEY_PROVIDER, null);
+    }
+
+    public abstract void startUpdates();
+
+    public abstract void stopUpdates();
+
+    protected void updateLocation(Location location) {
+        if (location != null && location.getTime() - lastUpdateTime >= period) {
+            Log.i(TAG, "location new");
+            lastUpdateTime = location.getTime();
+            listener.onPositionUpdate(new Position(deviceId, location, getBatteryLevel()));
+        } else {
+            Log.i(TAG, location != null ? "location old" : "location nil");
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    private double getBatteryLevel() {
+        if (android.os.Build.VERSION.SDK_INT > Build.VERSION_CODES.ECLAIR) {
+            Intent batteryIntent = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+            int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, 1);
+            return (level * 100.0) / scale;
+        } else {
+            return 0;
+        }
+    }
+
+}
---
+++ app/src/main/java/org/eventoverwatch/client/TrackingService.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2012 - 2015 Anton Tananaev (anton.tananaev@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.eventoverwatch.client;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TrackingService extends Service {
+
+    private static final String TAG = TrackingService.class.getSimpleName();
+    private static final int NOTIFICATION_ID = 1;
+
+    private TrackingController trackingController;
+
+    @SuppressWarnings("deprecation")
+    private static Notification createNotification(Context context) {
+        Intent notificationIntent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        Notification notification = new Notification(android.R.drawable.stat_notify_sync_noanim, null, 0);
+        try {
+            Method method = notification.getClass().getMethod("setLatestEventInfo", Context.class, CharSequence.class, CharSequence.class, PendingIntent.class);
+            try {
+                method.invoke(notification, context, context.getString(R.string.app_name), context.getString(R.string.settings_status_on_summary), pendingIntent);
+            } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+                Log.w(TAG, e);
+            }
+        } catch (SecurityException | NoSuchMethodException e) {
+            Log.w(TAG, e);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            notification.priority = Notification.PRIORITY_MIN;
+        }
+
+        return notification;
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    public static class HideNotificationService extends Service {
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public void onCreate() {
+            startForeground(NOTIFICATION_ID, createNotification(this));
+            stopForeground(true);
+        }
+
+        @Override
+        public int onStartCommand(Intent intent, int flags, int startId) {
+            stopSelfResult(startId);
+            return START_NOT_STICKY;
+        }
+    }
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "service create");
+        StatusActivity.addMessage(getString(R.string.status_service_create));
+
+        trackingController = new TrackingController(this);
+        trackingController.start();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            startForeground(NOTIFICATION_ID, createNotification(this));
+            startService(new Intent(this, HideNotificationService.class));
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onStart(Intent intent, int startId) {
+        if (intent != null) {
+            AutostartReceiver.completeWakefulIntent(intent);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        onStart(intent, startId);
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "service destroy");
+        StatusActivity.addMessage(getString(R.string.status_service_destroy));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            stopForeground(true);
+        }
+
+        if (trackingController != null) {
+            trackingController.stop();
+        }
+    }
+
+}
